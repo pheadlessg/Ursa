2127ea567e3a5127806be783229549ccd206aeb6
Sean Fisher <seanfisher@Seans-MacBook-Air.local>
2019-01-08 11:16:34 +0000

    added Drizzle, updated react App and index

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = { loading: true, drizzleState: null };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h1>
+          {this.state.loading
+            ? 'Loading Drizzle...'
+            : 'Drizzle loaded - check the console'}
+        </h1>
+      </>
     );
+  }
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+    this.unsubscribe = drizzle.store.subscribe(() => {
+      const drizzleState = drizzle.store.getState();
+      if (drizzleState.drizzleStatus.initialized) {
+        console.log(drizzleState);
+        this.setState({ loading: false, drizzleState });
+        console.log(this.state);
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 }
 
