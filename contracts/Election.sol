a410b2b6947669ae2bbae2dd63cb3b669f62eb57
Xavier Bruggen <xav@Xaviers-MacBook-Air.local>
2019-01-08 12:48:26 +0000

    Election contract and add candidates function

---
+++ contracts/Election.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+}
