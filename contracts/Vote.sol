7ab4569e3c35ab4069e425cc3f9b5ad91bfd8584
Xavier Bruggen <xtlbruggen@gmail.com>
2019-01-10 17:16:41 +0000

    election struct coming through in tests

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,18 +1,5 @@
 pragma solidity ^0.5.0;
 
-// contract Owned {
-//     address public owner;
-
-//     constructor () public {
-//         owner = msg.sender;
-//     }
-
-//     modifier onlyOwner {
-//         require(msg.sender == owner);
-//         _;
-//     }
-// }
-
 contract Vote {
 
     struct Election {
@@ -38,7 +25,8 @@ contract Vote {
     }
 
     uint public voterCount;
-    uint public candidatesCount;
+    // DO NOT REMOVE THIS CANDIDATESCOUNT
+    uint public candidatesCount = 33;
     uint public electionCount;
 
     mapping(uint => Election) public elections;
--- test/MyStringStore.test.js
+++ test/MyStringStore.test.js
@@ -1,40 +1,61 @@
-const Vote = artifacts.require("./Election.sol");
+const Vote = artifacts.require('./Vote.sol');
 
-contract("Vote", accounts => {
-  it("initializes with two candidates", () => {
+contract('Vote', accounts => {
+  // it("initializes with two candidates", () => {
+  //   return Vote.deployed()
+  //     .then(instance => {
+  //       return instance.candidatesCount();
+  //     })
+  //     .then(count => {
+  //       assert.equal(count, 2);
+  //     });
+  // });
+  // it("initalizes the candidates with the correct values", () => {
+  //   return Vote.deployed()
+  //     .then(instance => {
+  //       VoteInstance = instance;
+  //       return VoteInstance.candidates(1);
+  //     })
+  //     .then(candidate => {
+  //       assert.equal(candidate[0], 1, "contains the correct id");
+  //       assert.equal(
+  //         candidate[1],
+  //         "Anthony Applegate",
+  //         "contains the correct name"
+  //       );
+  //       assert.equal(candidate[2], 0, "contains the correct voteCount");
+  //     });
+  // });
+  // it("vote count increments + 1", async () => {
+  //   const instance = await Vote.deployed();
+  //   let candidate = await instance.candidates(1);
+  //   assert.equal(candidate.voteCount, 0);
+  //   await instance.incrementVote(1, 1);
+  //   candidate = await instance.candidates(1);
+  //   assert.equal(candidate.voteCount, 1);
+  // });
+  it('smoke test', async () => {
     return Vote.deployed()
       .then(instance => {
+        console.log(instance);
         return instance.candidatesCount();
       })
       .then(count => {
-        assert.equal(count, 2);
+        console.log(count);
+        assert.equal(count, 33);
       });
   });
-  it("initalizes the candidates with the correct values", () => {
+  it('initilizes a new election with the correct values', async () => {
     return Vote.deployed()
       .then(instance => {
-        VoteInstance = instance;
-        return VoteInstance.candidates(1);
-      })
-      .then(candidate => {
-        assert.equal(candidate[0], 1, "contains the correct id");
-        assert.equal(
-          candidate[1],
-          "Anthony Applegate",
-          "contains the correct name"
+        return instance.startElection(
+          '0x994dd176fa212730d290465e659a7c7d0549e384',
+          'Test Election',
+          5555
         );
-        assert.equal(candidate[2], 0, "contains the correct voteCount");
+      })
+      .then(election => {
+        console.log(election);
       });
-  });
-  it("vote count increments + 1", async () => {
-    const instance = await Vote.deployed();
-    let candidate = await instance.candidates(1);
-    assert.equal(candidate.voteCount, 0);
-    await instance.incrementVote(1, 1);
-    candidate = await instance.candidates(1);
-    assert.equal(candidate.voteCount, 1);
-  });
-  it("initilizes a new election with the correct values", async () => {
-    const instance = await Vote.deployed();
   });
 });
