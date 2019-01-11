9edbfc82421d2e546c5dc8954711c59ab3e8e9a6
Xavier Bruggen <xtlbruggen@gmail.com>
2019-01-11 00:13:30 +0000

    refactored contract function testing

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -4,18 +4,23 @@ contract Vote {
 
     string public testString = "Im here for testing, leave me be!";
     // please do not remove testString
-    uint public electionCount;
-    mapping(uint => Election) public elections;
 
     struct Election {
         address creator;
         string electionName;
         uint voteCount;
-        address[] voters;
         uint expirationTime;
         uint candidatesCount;
         mapping(address => Voter) votersInfo;
         mapping(uint => Candidate) candidates;
+    }
+
+    uint public electionCount;
+    mapping(uint => Election) public elections;
+
+    function startElection(address _creator, string memory _electionName, uint _expirationTime) public {
+        electionCount++;
+        elections[electionCount] = Election(_creator, _electionName, 0, _expirationTime, 0);
     }
 
     struct Candidate  {
@@ -32,38 +37,14 @@ contract Vote {
     uint public voterCount;
     uint public candidatesCount;
 
-    event VoteTracker(uint id, uint voteCount);
-
-    function startElection(address _creator, string memory _electionName, uint _expirationTime) public {
-        electionCount++;
-        address[] memory voters;
-        elections[electionCount] = Election(_creator, _electionName, 0, voters, _expirationTime, 0);
-    }
-
     function addElectionCandidate(uint _electionId, string memory _candidateName) public {
-        elections[_electionId].candidatesCount++;
-        uint i = elections[_electionId].candidatesCount;
-        Candidate memory newCand;
-        newCand.id = i;
-        newCand.name = _candidateName;
-        elections[_electionId].candidates[i] = newCand;
+        Election storage e = elections[_electionId];
+        e.candidates[e.candidatesCount++] = Candidate(e.candidatesCount, _candidateName, 0);
     }
 
-
-    // function addCandidate(string memory _name) public {
-    //     candidatesCount++;
-    //     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    // }
-
-    // function incrementVote(uint _id, uint _inc) public {
-    //     candidates[_id].voteCount = candidates[_id].voteCount + _inc;
-    //     emit VoteTracker(_id, candidates[_id].voteCount);
-    // }
-
-    // function addVoter(address _name) public {
-    //     voterCount++;
-    //     voters[voterCount] = Voter(voterCount, _name, false);
-    // }
+    function showElectionCandidates(uint _electionId) public returns (string memory name){
+        return elections[_electionId].candidates[1].name;
+    }
 
     constructor () public {
     }
--- test/MyStringStore.test.js
+++ test/MyStringStore.test.js
@@ -1,39 +1,6 @@
 const Vote = artifacts.require('./Vote.sol');
 
 contract('Vote', accounts => {
-  // it("initializes with two candidates", () => {
-  //   return Vote.deployed()
-  //     .then(instance => {
-  //       return instance.candidatesCount();
-  //     })
-  //     .then(count => {
-  //       assert.equal(count, 2);
-  //     });
-  // });
-  // it("initalizes the candidates with the correct values", () => {
-  //   return Vote.deployed()
-  //     .then(instance => {
-  //       VoteInstance = instance;
-  //       return VoteInstance.candidates(1);
-  //     })
-  //     .then(candidate => {
-  //       assert.equal(candidate[0], 1, "contains the correct id");
-  //       assert.equal(
-  //         candidate[1],
-  //         "Anthony Applegate",
-  //         "contains the correct name"
-  //       );
-  //       assert.equal(candidate[2], 0, "contains the correct voteCount");
-  //     });
-  // });
-  // it("vote count increments + 1", async () => {
-  //   const instance = await Vote.deployed();
-  //   let candidate = await instance.candidates(1);
-  //   assert.equal(candidate.voteCount, 0);
-  //   await instance.incrementVote(1, 1);
-  //   candidate = await instance.candidates(1);
-  //   assert.equal(candidate.voteCount, 1);
-  // });
   it('smoke test', async () => {
     return Vote.deployed()
       .then(instance => {
@@ -64,13 +31,18 @@ contract('Vote', accounts => {
       'voteCount',
       'candidatesCount'
     ]);
+    // the function changes letters to upper case!
+    expect(election.creator).to.eql(
+      '0x994DD176fA212730D290465e659a7c7D0549e384'
+    );
+    expect(election.electionName).to.eql('Test Election');
   });
   it('add a candidate to an election', async () => {
     const instance = await Vote.deployed();
     await instance.addElectionCandidate(1, 'Test Candidate');
-    const election = await instance.elections(1);
-    const cand = election.candidates;
-    console.log(cand);
-    expect(typeof election.candidatesCount).to.eql('object');
+    const candidates = await instance.showElectionCandidates(1);
+    console.log(
+      'this function needs to actually return the candidate names, maybe with events on the contract'
+    );
   });
 });
