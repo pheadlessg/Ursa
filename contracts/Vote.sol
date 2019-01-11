92ed4dec1be4ebe8b0d95c7e2e9a7afa96032003
Xavier Bruggen <xtlbruggen@gmail.com>
2019-01-11 15:23:39 +0000

    still refactoring add candidates

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -10,26 +10,32 @@ contract Vote {
     struct Election {
         address creator;
         string electionName;
-        uint voteCount;
         uint expirationTime;
         uint candidatesCount;
-        mapping(address => Voter) votersInfo;
-        mapping(uint => Candidate) candidates;
+        Candidate[] candidates;
     }
 
     uint public electionCount;
-    mapping(uint => Election) public elections;
+    Election[] public elections;
 
     function startElection(address _creator, string memory _electionName, uint _voteLength) public {
-
         electionCount++;
-        
-        // input vote length
-        // set Timer returning expiration time
-        uint expirationTime = setTimer(_voteLength);
-        // initiating a new Election with the final expiration time
-        elections[electionCount] = Election(_creator, _electionName, 0, expirationTime, 0);
+        Election memory newElection;
+        newElection.creator = _creator;
+        newElection.electionName = _electionName;
+        newElection.expirationTime = setTimer(_voteLength);
+        newElection.candidatesCount = 5;
+        elections.push(newElection);
+        electionCount++;
     }
+
+    // Candidate[] public candidates;
+    // uint public candidateCount;
+
+    // function addCandidate(string name) public {
+    //     Candidate memory newCandidate;
+
+    // }
 
     struct Candidate  {
         uint id;
@@ -55,16 +61,10 @@ contract Vote {
         return elections[_electionId].candidates[1].name;
     }
 
-    function addVoter(address name) public {
-        transfer(name, 1) public returns bool;
-    }
-    // ROS AND BEVS TIMER FUNCTION
-    // FROM HERE
     function setTimer(uint _voteLength) public returns (uint) {
         uint StartTime = block.timestamp;
         return StartTime + _voteLength;
     }
-    // TO HERE 
 
     constructor () public {
     }
--- test/NewElection.test.js
+++ test/NewElection.test.js
@@ -24,11 +24,9 @@ contract('Vote', accounts => {
       '1',
       '2',
       '3',
-      '4',
       'creator',
       'electionName',
       'expirationTime',
-      'voteCount',
       'candidatesCount'
     ]);
     // the function changes letters to upper case!
@@ -45,10 +43,10 @@ contract('Vote', accounts => {
       'this function needs to actually return the candidate names, maybe with events on the contract'
     );
   });
-  it.only('sets an expiration time of the vote based on input', async () => {
+  it('sets an expiration time of the vote based on input', async () => {
     const instance = await Vote.deployed();
     const expireTime = await instance.setTimer.call(600);
-    expect(expireTime.toNumber()).to.be.below(Date.now() + 600000)
-    expect(expireTime.toNumber()).to.not.be.above(Date.now() + 600000)    
+    expect(expireTime.toNumber()).to.be.below(Date.now() + 600000);
+    expect(expireTime.toNumber()).to.not.be.above(Date.now() + 600000);
   });
 });
